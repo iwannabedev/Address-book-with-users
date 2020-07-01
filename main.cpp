a834d79d2af801c2a44db0f2675ffb4c1f02c9b5
iWannaBe.dev <iwannabe.dev@gmail.com>
2020-07-01 20:11:46 +0100

    Add registerUser function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <conio.h>
+#include <vector>
 
 using namespace std;
+
+struct User {
+    short id;
+    string login, password;
+
+};
+
+void registerUser(vector<User> &users, short &numberOfUsers);
 
 int main(){
     system("chcp 1250");
     char choice = '0';
+    vector<User> users;
+    short numberOfUsers = 0;
 
     while (true) {
         system("cls");
@@ -18,7 +29,8 @@ int main(){
 
         switch (choice) {
             case '1':   break;
-            case '2':   break;
+            case '2':   registerUser(users, numberOfUsers);
+                        break;
             case '9':   exit(0);
                         break;
             default:    cout << '\n' << "Niepoprawny wybór. Spróbuj ponownie." << '\n';
@@ -26,4 +38,37 @@ int main(){
         }
 
     }
+}
+
+void registerUser(vector<User> &users, short &numberOfUsers) {
+    User registeredUser;
+    vector<User>::iterator itr = users.begin(), lastUserPosition = users.end();
+    string userName;
+    bool userExists = false;
+    cout << "Podaj nazwe uzytkownika: ";
+    cin >> userName;
+
+    userExists = (numberOfUsers) ? true:false;
+
+    while (userExists) {
+        for (itr; itr != lastUserPosition; ++itr) {
+            if (userName == itr->login) {
+                cout << "Taki uzytkownik istnieje. Wpisz inna nazwe uzytkownika: ";
+                cin >> userName;
+                itr = users.begin();
+            }
+        }
+        userExists = false;
+    }
+
+    registeredUser.id = ++numberOfUsers;
+    registeredUser.login = userName;
+
+    cout << "Podaj has³o: ";
+    cin >> registeredUser.password;
+    
+    users.emplace_back(registeredUser);
+    
+    cout << "Rejestracja zakoñczona" << endl;
+    system("pause");
 }
